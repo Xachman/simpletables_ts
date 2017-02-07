10669eb6d3d6cab05541376803a6a7b03bb0f2f5
xach <zironside@hotmail.com>
2017-02-07 06:48:21 -0500

    add admin html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,28 +12,6 @@ export class AppComponent {
   title = 'Application Title';
 	message = "Please add database";
 	constructor() {
-		this.getDatabase();
-		var db = new SQLite();
-		db.open();
-		try {
-			db.run("CREATE Table Test (a INT, b CHAR);");
-			db.prepareExecute("INSERT INTO Test VALUES (?,?)", [1, "test"]);
-			db.prepareExecute("INSERT INTO Test VALUES (?,?)", [12, 1]);
-			db.prepareExecute("INSERT INTO Test VALUES (?,?)", [12, 1]);
-			db.prepareExecute("INSERT INTO Test VALUES (?,?)", [12, 1]);
-			db.prepareExecute("INSERT INTO Test VALUES (?,?)", [12, 1]);
-			db.prepareExecute("INSERT INTO Test VALUES (?,?)", [12, 1]);
-			var stmt = db.prepare("SELECT * FROM Test WHERE a=$test");
-			stmt.bind({$test:12});
-			while(stmt.step()) {
-				var row = stmt.getAsObject();
-				console.log(row);
-			}
-
-			FileUtil.saveUintArray(db.export(), 'test.db');
-		} catch(e) {
-			console.log(e);
-		}
 	}
 	getDatabase() {
 		console.log("get the database");
