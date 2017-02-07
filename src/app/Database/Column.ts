320e429851220319e064588803122eb429170278
xach <zironside@hotmail.com>
2017-02-07 13:34:23 -0500

    add process type function

--- src/app/Database/Column.ts
+++ src/app/Database/Column.ts
@@ -1,15 +1,29 @@
-class Column {
+export class Column {
 	type: string;
 	name: string;
-	constructor(type:string , name: string) {
-		this.type = type;
+	private types = ["int", "char", "varchar", "text", "datetime", "time", "date"];
+
+	constructor(name: string, type: string) {
+		this.type = this.processType(type);
 		this.name = name;
-		console.log(type);
 	}
 	getName() {
 	   return this.name;
 	}
 	getType() {
 		return this.type;	
+	}
+	getTypes(): Array<string> {
+		return this.types;
+	}
+
+	private processType(type: string): string {
+		for(var i = 0; i < this.getTypes().length; i++) {
+			var typeCheck = this.getTypes()[i];
+			if(typeCheck.toLowerCase() == type.toLowerCase()) {
+				return type.toLowerCase();
+			}
+		}
+		throw new Error("Type not found");
 	}
 }
