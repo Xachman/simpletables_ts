ca624198929eba0bd9493d49bc9c2e70fa425a04
xach <zironside@hotmail.com>
2017-02-08 08:03:10 -0500

    add generateField function and test

--- src/app/Admin/Field.ts
+++ src/app/Admin/Field.ts
@@ -1,11 +1,27 @@
 import { Column } from "../Database/Column";
 
 export class Fields {
-	loadFieldMap(path: string) {
+	
+	private fieldsData: Array<FieldData>
 
+	constructor(fieldsData: Array<FieldData>) {
+		this.fieldsData = fieldsData;
 	}
 	generateField(column: Column) {
-		
+		for(var i = 0; i < this.fieldsData.length; i ++) {
+			var field = this.fieldsData[i];
+			if(column.getType() == field.type) {
+				return field.html_field;
+			}
+		}
+
+		throw new Error("No field html found");
 	}
+	
 }
 
+
+interface FieldData {
+	type: string;
+	html_field: string;
+}
