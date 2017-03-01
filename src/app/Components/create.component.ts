9ef06f0c870efb8225a33b403db8e9383d81a559
xach <zironside@hotmail.com>
2017-03-01 11:08:22 -0500

    add dynamic component and Tables object to create

--- src/app/Components/create.component.ts
+++ src/app/Components/create.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, Injector } from '@angular/core';
+import { Menu } from '../Admin/Menu';
+import {SQLite} from '../Database/SQLite';
+import {Tables} from '../Database/Tables';
 @Component({
   selector: 'create',
-  templateUrl: '../templates/create.component.html',
-  styleUrls: ['../assets/css/create.component.css']
+  templateUrl: '../../templates/create.component.html',
+  styleUrls: ['../../assets/css/create.component.css']
 })
-export class AppComponent {
-  title = 'Application Title';
-	message = "Please add database";
-	menu = new Menu();
-	constructor() {
+export class CreateComponent {
+	private db: SQLite;
+	private tables: Tables;
+	
+	constructor(private injector: Injector) {
+		this.db = this.injector.get('database');
+		console.log(this.db);
+		this.tables = new Tables(this.db);
 	}
 }
